e95c3040f9e8c78bb66e5321a05fb1dc488393e9
Eric Labrador Sainz <74373745+e1abrador@users.noreply.github.com>
2023-01-21 20:13:37 +0100

    added more extensions
    added more extensions

--- file_discovery.py
+++ file_discovery.py
@@ -68,42 +68,136 @@ class CMEModule:
                 found = True
                 context.log.highlight('Found .bak file: {}'.format(file))
 
-        # search for .tar files
-        search_tar_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.tar -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
-        search_tar_files_cmd = 'powershell.exe "{}"'.format(search_tar_files_payload)
-        search_tar_files_output = connection.execute(search_tar_files_cmd, True).split("\r\n")
+        # search for .env files
+        search_env_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.env -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_env_files_cmd = 'powershell.exe "{}"'.format(search_env_files_payload)
+        search_env_files_output = connection.execute(search_env_files_cmd, True).split("\r\n")
         found = False
-        for file in search_tar_files_output:
-            if '.tar' in file:
+        for file in search_env_files_output:
+            if '.env' in file:
                 found = True
-                context.log.highlight('Found .tar file: {}'.format(file))
+                context.log.highlight('Found .env file: {}'.format(file))
 
-        # search for .zip files
-        search_zip_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.zip -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
-        search_zip_files_cmd = 'powershell.exe "{}"'.format(search_zip_files_payload)
-        search_zip_files_output = connection.execute(search_zip_files_cmd, True).split("\r\n")
+        # search for .yml files
+        search_yml_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.yml -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_yml_files_cmd = 'powershell.exe "{}"'.format(search_yml_files_payload)
+        search_yml_files_output = connection.execute(search_yml_files_cmd, True).split("\r\n")
         found = False
-        for file in search_zip_files_output:
-            if '.zip' in file:
+        for file in search_yml_files_output:
+            if '.yml' in file:
                 found = True
-                context.log.highlight('Found .zip file: {}'.format(file))
+                context.log.highlight('Found .yml file: {}'.format(file))
+                
+        # search for .yaml files
+        search_yaml_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.yaml -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_yaml_files_cmd = 'powershell.exe "{}"'.format(search_yaml_files_payload)
+        search_yaml_files_output = connection.execute(search_yaml_files_cmd, True).split("\r\n")
+        found = False
+        for file in search_yaml_files_output:
+            if '.yaml' in file:
+                found = True
+                context.log.highlight('Found .yaml file: {}'.format(file))                
 
-        # search for .rar files
-        search_rar_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.rar -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
-        search_rar_files_cmd = 'powershell.exe "{}"'.format(search_rar_files_payload)
-        search_rar_files_output = connection.execute(search_rar_files_cmd, True).split("\r\n")
+        # search for .properties files
+        search_properties_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.properties -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_properties_files_cmd = 'powershell.exe "{}"'.format(search_properties_files_payload)
+        search_properties_files_output = connection.execute(search_properties_files_cmd, True).split("\r\n")
         found = False
-        for file in search_rar_files_output:
-            if '.rar' in file:
+        for file in search_properties_files_output:
+            if '.properties' in file:
                 found = True
-                context.log.highlight('Found .rar file: {}'.format(file))
+                context.log.highlight('Found .properties file: {}'.format(file))                
 
-        # search for .7z files
-        search_7z_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.7z -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
-        search_7z_files_cmd = 'powershell.exe "{}"'.format(search_7z_files_payload)
-        search_7z_files_output = connection.execute(search_7z_files_cmd, True).split("\r\n")
+        # search for .ini files
+        search_ini_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.ini -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_ini_files_cmd = 'powershell.exe "{}"'.format(search_ini_files_payload)
+        search_ini_files_output = connection.execute(search_ini_files_cmd, True).split("\r\n")
         found = False
-        for file in search_7z_files_output:
-            if '.7z' in file:
+        for file in search_ini_files_output:
+            if '.ini' in file:
                 found = True
-                context.log.highlight('Found .7z file: {}'.format(file))
+                context.log.highlight('Found .ini file: {}'.format(file))    
+
+        # search for .config files
+        search_config_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.config -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_config_files_cmd = 'powershell.exe "{}"'.format(search_config_files_payload)
+        search_config_files_output = connection.execute(search_config_files_cmd, True).split("\r\n")
+        found = False
+        for file in search_config_files_output:
+            if '.config' in file:
+                found = True
+                context.log.highlight('Found .config file: {}'.format(file)) 
+
+        # search for .cfg files
+        search_cfg_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.cfg -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_cfg_files_cmd = 'powershell.exe "{}"'.format(search_cfg_files_payload)
+        search_cfg_files_output = connection.execute(search_cfg_files_cmd, True).split("\r\n")
+        found = False
+        for file in search_cfg_files_output:
+            if '.cfg' in file:
+                found = True
+                context.log.highlight('Found .cfg file: {}'.format(file))    
+
+                
+        # search for .conf files
+        search_conf_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.conf -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_conf_files_cmd = 'powershell.exe "{}"'.format(search_conf_files_payload)
+        search_conf_files_output = connection.execute(search_conf_files_cmd, True).split("\r\n")
+        found = False
+        for file in search_conf_files_output:
+            if '.conf' in file:
+                found = True
+                context.log.highlight('Found .conf file: {}'.format(file))    
+                
+        # search for .cnf files
+        search_cnf_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.cnf -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_cnf_files_cmd = 'powershell.exe "{}"'.format(search_cnf_files_payload)
+        search_cnf_files_output = connection.execute(search_cnf_files_cmd, True).split("\r\n")
+        found = False
+        for file in search_cnf_files_output:
+            if '.cnf' in file:
+                found = True
+                context.log.highlight('Found .cnf file: {}'.format(file))   
+                
+        # search for .plist files
+        search_plist_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.plist -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_plist_files_cmd = 'powershell.exe "{}"'.format(search_plist_files_payload)
+        search_plist_files_output = connection.execute(search_plist_files_cmd, True).split("\r\n")
+        found = False
+        for file in search_plist_files_output:
+            if '.plist' in file:
+                found = True
+                context.log.highlight('Found .plist file: {}'.format(file))   
+                
+                
+        # search for .key files
+        search_key_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.key -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_key_files_cmd = 'powershell.exe "{}"'.format(search_key_files_payload)
+        search_key_files_output = connection.execute(search_key_files_cmd, True).split("\r\n")
+        found = False
+        for file in search_key_files_output:
+            if '.key' in file:
+                found = True
+                context.log.highlight('Found .key file: {}'.format(file))   
+                
+        # search for .netrc files
+        search_netrc_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.netrc -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_netrc_files_cmd = 'powershell.exe "{}"'.format(search_netrc_files_payload)
+        search_netrc_files_output = connection.execute(search_netrc_files_cmd, True).split("\r\n")
+        found = False
+        for file in search_netrc_files_output:
+            if '.netrc' in file:
+                found = True
+                context.log.highlight('Found .netrc file: {}'.format(file)) 
+                
+                
+        # search for .php files
+        search_php_files_payload = "Get-ChildItem -Path {} -Recurse -Force -Include *.php -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName".format(self.search_path)
+        search_php_files_cmd = 'powershell.exe "{}"'.format(search_php_files_payload)
+        search_php_files_output = connection.execute(search_php_files_cmd, True).split("\r\n")
+        found = False
+        for file in search_php_files_output:
+            if '.php' in file:
+                found = True
+                context.log.highlight('Found .php file: {}'.format(file)) 
+                
